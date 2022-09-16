ca917c3bd9693f0e6036c2890b4a4be948101d0a
vishal Thakur <vish75810@gmail.com>
2022-09-17 02:11:14 +0530

    Working with Stripe Token and Cards Api

--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -19,5 +19,22 @@ class StripeService
         end
         stripe_customer
       end
+      def create_card_token(params)
+        Stripe::Token.create({
+            card: {
+                number: params[:card_number].to_s,
+                exp_month: params[:exp_month],
+                exp_year: params[:exp_year],
+                cvc: params[:cvc] 
+            }
+        })
+        end
+        def create_stripe_customer_card(params, stripe_customer)
+            token = create_card_token(params)
+            Stripe::Customer.create_source(
+                 stripe_customer.id,
+                { source: token.id }
+            )
+        end
 
 end
