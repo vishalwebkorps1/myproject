6787f0cdce3151569ef0b502e079cc09d05b53a1
vishal Thakur <vish75810@gmail.com>
2022-09-21 18:53:16 +0530

    Understanding Difference Between Bugs and Errors

--- app/controllers/refunds_controller.rb
+++ app/controllers/refunds_controller.rb
@@ -11,12 +11,18 @@ class RefundsController < ApplicationController
         booking = Booking.find_by(order_number: params[:refund][:order_number])
 
         if booking.present?
+           if booking.is_refundable?
             @refund = Refund.create(
                 customer_id: booking.customer_id,
                 booking_id: booking.id,
                 state: 'accepted'
             )
             redirect_to edit_refund_path(@refund), notice: 'Your booking request s valid. Please fill other details to process your refunds request.'
+        else
+            redirect_to new_refund_path, alert: "you are requesting refund for a past 
+            workshop. Refunds are only allowed for workshops which are to be taken in 
+            the future."
+        end
         else
             redirect_to new_refund_path, alert: "You provided an invalid booking ID #{
                 params[:refund][:order_number]}. We found no booking with this booking id. Please provide valid booking Id."
