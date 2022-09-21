5a58800e79d5c2e32af25405401773a4ddab89d1
vishal Thakur <vish75810@gmail.com>
2022-09-22 02:27:06 +0530

    Processing Partial Refunds on Stripe

--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -46,4 +46,11 @@ class StripeService
             customer: stripe_customer_id,
             description: "Amount $#{amount_to_be_paid} charged for #{workshop.name}"})
         end
+
+        def create_stripe_refund(stripe_charge_id, amount)
+            Stripe::Refund.create({
+                charge: stripe_charge_id,
+                amount: amount * 100
+            })
+       end
 end
